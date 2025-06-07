82943d906e2f11c67b2c635dcf5175fc2950f688
rickmugridge <rick.mugridge@gmail.com>
2025-06-07 14:34:43 +1200

    Refine box_builder

--- src/builder/box_builder.rs
+++ src/builder/box_builder.rs
@@ -1,5 +1,5 @@
-use bevy::color::{Color, Srgba};
-use bevy::prelude::{BorderColor, Commands, Entity, Node, UiRect};
+use bevy::color::Color;
+use bevy::prelude::{BackgroundColor, Commands, Entity, Node, UiRect};
 
 pub struct BoxBuilder {
     node: Node,
@@ -10,23 +10,21 @@ impl Default for BoxBuilder {
     fn default() -> Self {
         Self {
             node: Node::default(),
-            border_color: Color::default(),
+            border_color: Color::WHITE,
         }
     }
 }
 
 impl BoxBuilder {
-    pub fn border(mut self, border: UiRect, border_color: Color) -> Self {
+    pub fn new(mut self, border: UiRect, border_color: Color) -> Self {
         self.node.border = border;
         self.border_color = border_color;
         self
     }
 
     pub fn build_and_spawn(self, commands: &mut Commands) -> Entity {
-        let id = commands.spawn(self.node).id();
         commands
-            .entity(id)
-            .insert(BorderColor(Color::Srgba(Srgba::RED)));
-        id
+            .spawn((self.node, BackgroundColor(self.border_color)))
+            .id()
     }
 }
